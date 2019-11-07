94f2fe0237e2aebe09a4b2e29124aa4b4e9ab2ca
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-07 14:59:20 +0100

    Kickstart new service poller repository

---
+++ src/main/java/com/livi/servicepoller/package-info.java
@@ -0,0 +1,1 @@
+package com.livi.servicepoller;
---
+++ src/test/java/com/livi/servicepoller/CanaryTest.java
@@ -0,0 +1,12 @@
+package com.livi.servicepoller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CanaryTest {
+    @Test
+    public void canaryTest() {
+        assertTrue(true);
+    }
+}
