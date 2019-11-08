74b0b9ca4bedb1d9960b0426862ba35426bf6d92
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-08 12:10:47 +0100

    Catch exceptions in Application and log the error

--- application/src/main/java/com/livi/servicepoller/application/Application.java
+++ application/src/main/java/com/livi/servicepoller/application/Application.java
@@ -2,7 +2,10 @@ package com.livi.servicepoller.application;
 
 
 import com.livi.servicepoller.application.inmemory.*;
-import com.livi.servicepoller.domain.*;
+import com.livi.servicepoller.domain.ServiceDefinition;
+import com.livi.servicepoller.domain.ServiceMonitor;
+import com.livi.servicepoller.domain.ServiceRegistry;
+import com.livi.servicepoller.domain.Status;
 import com.livi.servicepoller.domain.common.ServicePollerInstrumentation;
 
 import java.util.Scanner;
@@ -21,39 +24,43 @@ public final class Application {
         System.out.println("Welcome to the simple service poller interface:");
         System.out.println("Please see app.log file to see service health status changes.");
         while (true) {
-            System.out.println("");
-            System.out.println("1 - to add a service");
-            System.out.println("2 - to remove a service");
-            System.out.println("3 - to list all services");
-            System.out.println("4 - find the health status of a service");
-            System.out.println("5 - to exit");
-            final String n = scanner.next();
-            switch (n) {
-                case "1":
-                    System.out.println("========== You want to add a service ==========");
-                    final String serviceURLToAdd = askUserForTheServiceURL();
-                    serviceRegistry.register(new ServiceDefinition(serviceURLToAdd));
-                    break;
-                case "2":
-                    System.out.println("========== You want to remove a service ==========");
-                    System.out.println("This functionality is not implemented yet");
-                    break;
-                case "3":
-                    System.out.println("========== You want to list all services ==========");
-                    listAllServicesToConsole(serviceRegistry.findAll());
-                    break;
-                case "4":
-                    System.out.println("========== You want to find the health status of a service ==========");
-                    final String serviceURLToRetrieveStatus = askUserForTheServiceURL();
-                    final Status status = serviceStatuses.find(serviceURLToRetrieveStatus);
-                    System.out.println("The service " + serviceURLToRetrieveStatus + " is " + status);
-                    break;
-                case "5":
-                    System.out.println("Goodbye!");
-                    simpleCron.shutdown();
-                    System.exit(0);
-                default:
-                    System.out.println("I did not understand please try again");
+            try {
+                System.out.println("");
+                System.out.println("1 - to add a service");
+                System.out.println("2 - to remove a service");
+                System.out.println("3 - to list all services");
+                System.out.println("4 - find the health status of a service");
+                System.out.println("5 - to exit");
+                final String n = scanner.next();
+                switch (n) {
+                    case "1":
+                        System.out.println("========== You want to add a service ==========");
+                        final String serviceURLToAdd = askUserForTheServiceURL();
+                        serviceRegistry.register(new ServiceDefinition(serviceURLToAdd));
+                        break;
+                    case "2":
+                        System.out.println("========== You want to remove a service ==========");
+                        System.out.println("This functionality is not implemented yet");
+                        break;
+                    case "3":
+                        System.out.println("========== You want to list all services ==========");
+                        listAllServicesToConsole(serviceRegistry.findAll());
+                        break;
+                    case "4":
+                        System.out.println("========== You want to find the health status of a service ==========");
+                        final String serviceURLToRetrieveStatus = askUserForTheServiceURL();
+                        final Status status = serviceStatuses.find(serviceURLToRetrieveStatus);
+                        System.out.println("The service " + serviceURLToRetrieveStatus + " is " + status);
+                        break;
+                    case "5":
+                        System.out.println("Goodbye!");
+                        simpleCron.shutdown();
+                        System.exit(0);
+                    default:
+                        System.out.println("I did not understand please try again");
+                }
+            } catch (Exception e) {
+                System.err.println("An exception occured : " + e.getMessage());
             }
         }
 
